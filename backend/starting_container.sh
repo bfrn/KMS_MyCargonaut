814c210d6e848dbde9bd03c6c3c84626255d9b93
Simon Stockhause <simon.stockhause@mni.thm.de>
2018-12-07 18:59:18 +0100

    kek travis

--- backend/starting_container.sh
+++ backend/starting_container.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-docker run -p 8080:8080 -it -e DBUSERNAME=simon -e DBPASSWORD=123 --network node_server -v $PWD/data:/app --name nodeServ --rm nodeserver
+docker run -p 8080:8080 -it --network node_server -v $PWD/data:/app --name nodeServ --rm nodeserver
 
