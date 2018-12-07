356dea217db307afe8a0de546850662896418298
Simon Stockhause <simon.stockhause@mni.thm.de>
2018-12-07 19:14:41 +0100

    kek travis compose

--- backend/data/test/user.test.js
+++ backend/data/test/user.test.js
@@ -29,18 +29,10 @@ describe('Test the User API', ()=> {
         })
     })
     describe('\POST /user/register', ()=>{
-        it('should create 3 users, which leads to exactly 3 entrys', (done) => {
+        it('should create 1 user, which leads to exactly 1 entry with correct properties', (done) => {
             let user1 = {
                 username: "simon",
                 password: "123"
-            }
-            let user2 = {
-                username: "björn",
-                password: "456"
-            }
-            let user3 = {
-                username: "paul",
-                password: "789"
             }
             chai.request(app)
                 .post('/user/register')
@@ -48,32 +40,13 @@ describe('Test the User API', ()=> {
                 .end((err, res) => {
                     res.body.should.have.property('success').eql('user successfully created')
                 })
-            setTimeout(1000);
-            chai.request(app)
-                .post('/user/register')
-                .send(user2)
-                .end((err, res) => {
-                    res.body.should.have.property('success').eql('user successfully created')
-                })
-            setTimeout(1000);
-            chai.request(app)
-                .post('/user/register')
-                .send(user3)
-                .end((err, res) => {
-                    res.body.should.have.property('success').eql('user successfully created')
-                })
-            setTimeout(1000);
             chai.request(app)
                 .get('/user')
                 .end((err, res) => {
                     res.body.should.be.a('array')
-                    res.body.length.should.be.eql(3)
+                    res.body.length.should.be.eql(1)
                     res.body[0].should.have.property('username').eql('simon')
                     res.body[0].should.have.property('password').eql('123')
-                    res.body[1].should.have.property('username').eql('björn')
-                    res.body[1].should.have.property('password').eql('456')
-                    res.body[2].should.have.property('username').eql('paul')
-                    res.body[2].should.have.property('password').eql('789')
                     done()
                 })
         })
