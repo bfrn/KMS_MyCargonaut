7e6afc8fa49b928ecd49883f3d388a8a8a3b091f
Simon Stockhause <simon.stockhause@mni.thm.de>
2018-12-14 21:42:21 +0100

    updated starting script

--- development_start.sh
+++ development_start.sh
@@ -2,7 +2,7 @@
 
 # Start Database
 echo 'Starting database'
-docker run -p 27017:27017 --rm  -d --network node_server -v $PWD/database/data:/data/db --name nodeDatabase mongo:latest 
+docker run -p 27017:27017 --rm  -d --network node_server -v $PWD/database/data:/data/db --name nodeDatabase mongo:latest
 
 sleep 5s
 
@@ -12,5 +12,5 @@ docker build -t nodeserver_tmp -f ./backend/Dockerfile_production ./backend
 docker run -p 8080:8080 -d --network node_server -v $PWD/backend/data:/app --name nodeserver_tmp --rm nodeserver_tmp
 
 echo 'Starting Frontend'
-cd frontend && npm install && npm run serve -- --port 4000
+cd frontendAngular && ng serve
 wait
