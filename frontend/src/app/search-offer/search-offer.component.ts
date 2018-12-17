033db3c3dce24714e604985d94ebf3a91973f63a
PaulinaNiederhoefer <paulina.niederhoefer@mni.thm.de>
2018-12-17 21:31:29 +0100

    - new components - updated package.json
    Signed-off-by: PaulinaNiederhoefer <paulina.niederhoefer@mni.thm.de>

---
+++ frontend/src/app/search-offer/search-offer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-offer',
+  templateUrl: './search-offer.component.html',
+  styleUrls: ['./search-offer.component.scss']
+})
+export class SearchOfferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
