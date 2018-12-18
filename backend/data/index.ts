9c9e08db599ca2057280dcb932f6d206a2bfd9c3
Björn Franke <bjoern.franke@mni.thm.de>
2018-12-18 09:55:06 +0100

    fixed GET user/:id problem

---
+++ backend/data/index.ts
@@ -0,0 +1,8 @@
+import app from './src/app'
+
+const PORT = 8080
+
+app.listen(PORT, (err)=>{
+   if(err) return console.log("Error Server Start")
+   return console.log('Express Server Listing jsdsd on http://Localhost:'+PORT)
+})
--- backend/data/src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from './app'
-
-const PORT = 8080
-
-app.listen(PORT, (err)=>{
-   if(err) return console.log("Error Server Start")
-   return console.log('Express Server Listing on http://Localhost:'+PORT)
-})
