c4050a036ab04bc3453232089039eaa605bdc10f
PaulinaNiederhoefer <paulina.niederhoefer@mni.thm.de>
2018-12-19 15:39:34 +0100

    - new components
    Signed-off-by: PaulinaNiederhoefer <paulina.niederhoefer@mni.thm.de>

---
+++ frontendAngular/src/app/classes/drivingOffers.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var DrivingOffers = /** @class */ (function () {
+    function DrivingOffers() {
+    }
+    return DrivingOffers;
+}());
+exports.DrivingOffers = DrivingOffers;
