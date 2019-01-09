ec15fbc0fad4f5ca0422a657f13dd68df80e168c
Simon Stockhause <simon.stockhause@mni.thm.de>
2019-01-09 11:58:22 +0100

    removed dists

--- backend/data/dist/controllers/drivingOffers.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-let driving = new Schema({
-    email: { type: String, required: true },
-});
-// Export the User model
-module.exports = mongoose.model('Driving', driving);
-//# sourceMappingURL=drivingOffers.model.js.map
